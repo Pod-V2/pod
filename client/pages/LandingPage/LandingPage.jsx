b84ad58feaae1aec13bc1127e149fd987fd707b0
Jesse Chou <jessechou7@gmail.com>
2023-10-21 13:59:56 -0400

    Initial revamp

--- client/pages/LandingPage/LandingPage.jsx
+++ client/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import * as React from 'react';
 
 export default function LandingPage () {
+
+  const LandingPageNav = ['Login', 'Signup']
+
   return (
-    <div>
-      <nav>
-        <h1></h1>
+    <div style={{backgroundImage: `url:("https://www.searchenginejournal.com/wp-content/uploads/2023/04/marketplace-ecommerce-sellers-6436a446a2a18-sej-1280x720.png")`}}>
+      <nav className="landingPageNav">
+        
       </nav>
+      <h1>Welcome to Pod</h1>
+      <h2>A marketplace connecting buyers and sellers for used goods</h2>
     </div>
   )
 }
