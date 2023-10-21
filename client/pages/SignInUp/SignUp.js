6d1e94275c58a1cd5c4a422192e2ed472957e1d4
Francisco Lopez <franciscoglopez49@gmail.com>
2023-10-21 13:56:24 -0400

    Replaced components with Material UI

--- client/pages/SignInUp/SignUp.js
+++ client/pages/SignInUp/SignUp.js
@@ -1,104 +1,61 @@
 import React from "react";
 import { useState } from "react";
+import styled from "styled-components";
+import { TextField, Button, FormControl } from '@material-ui/core';
 
 const SignUp = () => {
 
   const [email, setEmail] = useState('')
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setlastName] = useState('')
-  const [username, setUsername] = useState('')
-  const [city, setCity] = useState('')
-  const [zip, setZip] = useState('')
-  const [state, setState] = useState('')
-  const [phoneNumber, setPhoneNumber] = useState('')
-  const [address, setaddress] = useState('')
+  const [fullname, setfullname] = useState('')
   const [password, setPassword] = useState('')
+  // const [username, setUsername] = useState('')
+  // const [lastName, setlastName] = useState('')
+  // const [city, setCity] = useState('')
+  // const [zip, setZip] = useState('')
+  // const [state, setState] = useState('')
+  // const [phoneNumber, setPhoneNumber] = useState('')
+  // const [address, setaddress] = useState('')
+  
 
   const handleSubmit = async(e) =>{
     e.preventDefault()
   }
-  console.log('test')
+  // console.log('test')
   return (
-    <div>
-      <div>
-      <h3 >Signup</h3>
-      <form onSubmit={handleSubmit}>
-      <div className="signup">
-      <label>First Name: </label>
-        <input type="text"
+  <div>
+    <StyledH3 >New here? Sign Up!</StyledH3>
+    <StyledContainer id='Signup Container'>
+      <FormControl onSubmit={handleSubmit}>
+        <TextField label="Full Name" variant="filled" type="text"
           onChange={e => setFirstName(e.target.value)}
           value={firstName}
         />
-      </div>
-      <div className="signup">
-      <label>Last Name: </label>
-        <input type="text"
-          onChange={e => setLastName(e.target.value)}
-          value={lastName}
-        />
-      </div>
-      <div className="signup">
-      <label>Address: </label>
-        <input type="text"
-          onChange={e => setAddress(e.target.value)}
-          value={address}
-        />
-      </div>
-      <div className="signup">
-      <label>City: </label>
-        <input type="text"
-          onChange={e => setCity(e.target.value)}
-          value={city}
-        />
-      </div>
-      <div className="signup">
-      <label>State: </label>
-        <input type="email"
-          onChange={e => setState(e.target.value)}
-          value={state}
-        />
-      </div>
-      <div className="signup">
-      <label>Zipcode: </label>
-        <input type="number"
-          onChange={e => setZip(e.target.value)}
-          value={zip}
-        />
-      </div>
-      <div className="signup">
-        <label>Phone#: </label>
-          <input type="number"
-            onChange={e => setPhoneNumber(e.target.value)}
-            value={phoneNumber}
-          />
-      </div>
-      <div className="signup">
-      <label>Email: </label>
-        <input type="email"
+        <TextField label="Email" variant="filled" type="email"
           onChange={e => setEmail(e.target.value)}
           value={email}
         />
-      </div>
-      <div className="signup">
-      <label>Username: </label>
-        <input type="text"
-          onChange={e => setUsername(e.target.value)}
-          value={username}
-        />
-      </div>
-      <div className="signup">
-      <label>Password: </label>
-        <input type="password"
+        <TextField label="Password" variant="filled" type="password"
           onChange={e => setPassword(e.target.value)}
           value={password}
         />
-      </div>
-        
-        <button className="signup">Signup</button>
-      </form>
-    </div>
-    </div>
+        <Button className="signup">Signup</Button>
+      </FormControl>
+    </StyledContainer>
+
+  </div>
   );
 }
+
+const StyledContainer = styled.div`
+  display: grid;
+  justify-content: center;
+  min-height: 500px
+`
+
+const StyledH3 = styled.h3`
+  display: grid;
+  justify-content: center;
+  text-align: center
+`
  
 export default SignUp;
