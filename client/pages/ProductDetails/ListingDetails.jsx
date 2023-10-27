c31333a3f0f8dc32016e4cca8842f03e950327ec
Jesse Chou <jessechou7@gmail.com>
2023-10-26 22:43:11 -0400

    refactor: cleaned up UI

--- client/pages/ProductDetails/ListingDetails.jsx
+++ client/pages/ProductDetails/ListingDetails.jsx
@@ -1,69 +1,88 @@
-import React, { useEffect, useState } from 'react';
-import { CardContent, FormControl, FormLabel } from '@mui/material';
+import React from 'react';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import PrimarySearchAppBar from '../../common/Header.jsx';
 import { useQuery } from '@tanstack/react-query';
-import { CardMedia } from '@material-ui/core';
 import { useParams } from 'react-router-dom';
-import { ListingText } from './components/ListingText.jsx'
+import { useNavigate } from 'react-router-dom';
+import { ListingText } from './components/ListingText.jsx';
 
-/**
- * Product creation details page
- * @param {*} props no props needed
- * @returns
- */
-export const ListingDetails = (props) => {
-  const [imageUrl, setImageUrl] = useState('');
+export const ListingDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
+
   const { isPending, error, data } = useQuery({
     queryKey: ['repoData'],
     refetchOnWindowFocus: false,
-    queryFn: () => fetch(`http://localhost:3000/api/listing/${id}`).then((res) => res.json(),).then((res) => {
-      console.log(res);
-      return res;
-    })
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3000/api/listing/id/${id}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    }
   });
 
   if (isPending) {
-    return (<>
-      <PrimarySearchAppBar/>
-      <div>'Loading...'</div>
-    </>);
+    return (
+      <>
+        <PrimarySearchAppBar />
+        <Box m={2} display="flex" justifyContent="center">
+          <Typography variant="h4">Loading...</Typography>
+        </Box>
+      </>
+    );
   }
-  if (error) return 'An error has occurred in fetching listings: ' + error.message;
+
+  if (error) {
+    return (
+      <>
+        <PrimarySearchAppBar />
+        <Box m={2} display="flex" justifyContent="center">
+          <Typography variant="h4">
+            An error has occurred in fetching listings: {error.message}
+          </Typography>
+        </Box>
+      </>
+    );
+  }
 
   return (
     <>
       <PrimarySearchAppBar />
-      <Box
-        sx={{ m: 1 }}
-        flexDirection="column"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Box sx={{ m: 1 }}>
-          <h1>Listing Details</h1>
-        </Box>
-        <Card sx={{ display: 'flex' }}>
+      <Box m={2} display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h4" gutterBottom>
+          Listing Details
+        </Typography>
+        <Card>
           <Grid container>
             <Grid item xs={12} md={6}>
               <CardMedia
                 component="img"
                 image={data.img_url}
-                alt="green iguana"
-                sx={{ maxHeight: 600 }}
+                alt="Listing Image"
+                style={{ maxHeight: '500px', width: '100%', objectFit: 'contain' }}
               />
             </Grid>
             <Grid item xs={12} md={6}>
-              <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                <ListingText data={data}></ListingText>
-                <Button size="large" color="primary">
-                  Add to Cart
-                </Button>
+              <CardContent>
+                <ListingText data={data} />
+                <Box mt={2}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    fullWidth
+                    onClick={() => navigate('/cart')}
+                  >
+                    Add to Cart
+                  </Button>
+                </Box>
               </CardContent>
             </Grid>
           </Grid>
