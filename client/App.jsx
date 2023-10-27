e8a72eec8115295531f06ea73757d59ba8de23f2
Jesse Chou <jessechou7@gmail.com>
2023-10-26 22:01:45 -0400

    feature: modified app.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { Navigate, BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.scss";
 import Pages from "./pages/pageLayout/Layout";
-import { AllListings } from "./pages/AllListings/AllListings.jsx";
-import Login from "./pages/SignInUp/Login";
-import SignUp from "./pages/SignInUp/SignUp";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import styled from "styled-components";
 
 const App = () => {
   const queryClient = new QueryClient();
