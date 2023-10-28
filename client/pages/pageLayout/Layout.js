2d92062d2256da4483358951e8ec33a4a38d07e7
Jesse Chou <75198150+jesse-chou@users.noreply.github.com>
2023-10-28 13:28:25 -0400

    Update Layout.js

--- client/pages/pageLayout/Layout.js
+++ client/pages/pageLayout/Layout.js
@@ -35,12 +35,10 @@ const Pages = () => {
           <Route path ="/home" element={<Home />} />
           <Route path ='/login' element={<Login/>}/>
           <Route path ='/signup' element={<SignUp/>}/>
-          <Route path ='/listing/' element={<AllListings/>}/>
-          <Route path ='/listing//:id' element={<AllListings/>}/>
+          <Route path ='/listing/:id' element={<AllListings/>}/>
           <Route path ='/listing/create' element={<ProductDetails/>}/>
           <Route path="/listing/details/:id" element={<ListingDetails/>} />
           <Route path="/cart"  element={<Cart/>}/>
-          <Route path="/ListingsByCategory/:category" element={<ListingsByCategory/>}/>
           <Route path="/ListingsByCategory/:category" element={<ListingsByCategory/>}/>
         </Routes>
       </Router>
