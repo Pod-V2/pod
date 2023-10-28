53b1e28e70bf954e900ff3b6fd1a7a38741d64bc
rswdch <ryan.sawadichai@outlook.com>
2023-10-28 13:45:02 -0600

    Styled categories by listing

--- client/pages/Home/ListingsByCategory.jsx
+++ client/pages/Home/ListingsByCategory.jsx
@@ -6,11 +6,28 @@ import { BiSearch } from "react-icons/bi";
 import styled from "styled-components";
 import Footer from "../../common/Footer";
 import Header from "../../common/Header.jsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 /**
  * Redirect to this page upon clicking one of the category buttons from the categories component, GET the listings from the category that was drilled
  * down.
  */
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    minWidth: 345,
+    minHeight: "100%",
+  },
+  media: {
+    height: 240,
+  },
+});
 
 const ListingsByCategory = (props) => {
   //send POST request to the backend using props.categoryName and then display?
@@ -18,53 +35,75 @@ const ListingsByCategory = (props) => {
   const { category } = useParams();
   const [listings, setListings] = useState([]);
   const navigate = useNavigate();
-
-  function handleClickListing(i) {
-    navigate(`/listing/details/${listings[i].listingid}`);
-  }
+  const classes = useStyles();
 
   useEffect(() => {
     fetch(`/api/listing/category/${category}`)
       .then((data) => data.json())
       .then((data) => {
-        console.log(
-          `Here is the GET request to /category/${category}: ${data}`
-        );
+        console.log(`Here is the GET request to /category/${category}`);
+        console.log(data);
         console.log(Array.isArray(data));
         setListings(data);
         console.log(`Listing array is currently: ${listings}`);
       });
   }, []);
 
+  function handleClickListing(i) {
+    navigate(`/listing/details/${listings[i].listingid}`);
+  }
+
   return (
-    <>
+    <div>
       <Header />
       <Container>
         {listings.map((element, i) => {
           return (
-            <Box listingName={element.listing}>
-              <div className="listing">
-                <div className="listingCard">
-                  <img></img>
-                  {/* <FaRegBookmark />
-              <FaFireAlt /> */}
-                </div>
-              </div>
-              <p></p>
-              <p></p>
+            <Card className={classes.root}>
+              <CardActionArea onClick={() => handleClickListing(i)}>
+                <CardMedia
+                  component="img"
+                  className={classes.media}
+                  src={element.img_url}
+                  title={element.listing}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {element.listing}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {element.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            // <Box listingName={element.listing}>
+            //   <div className="listing">
+            //     <div className="listingCard">
+            //       <img src={element.img_url}></img>
+            //       {/* <FaRegBookmark />
+            //   <FaFireAlt /> */}
+            //     </div>
+            //   </div>
+            //   <p></p>
+            //   <p></p>
 
-              <Button
-                onClick={() => {
-                  handleClickListing(i);
-                }}
-              >
-                {element.listing}
-              </Button>
-            </Box>
+            //   <Button
+            //     onClick={() => {
+            //       handleClickListing(i);
+            //     }}
+            //   >
+            //     {element.listing}
+            //   </Button>
+            // </Box>
           );
         })}
       </Container>
-    </>
+    </div>
   );
 };
 
@@ -77,13 +116,23 @@ const Text = styled.div`
 const Container = styled.div`
   display: grid;
   justify-content: center;
-  grid-template-columns: repeat(4, minmax(50px, 1fr));
-  grid-gap: 5rem;
+  align-items: center;
+  grid-gap: 1rem;
   width: fit-content;
   align-items: space-between;
-  @media (max-width: 1020px) {
-    display: grid;
+  margin: auto;
+  margin-top: 25px;
+  @media (max-width: 400px) {
     grid-template-columns: repeat(2, minmax(50px, 1fr));
+  }
+  @media (min-width: 900px) {
+    grid-template-columns: repeat(3, minmax(50px, 1fr));
+  }
+  @media (min-width: 1200px) {
+    grid-template-columns: repeat(4, minmax(50px, 1fr));
+  }
+  @media (min-width: 2600px) {
+    grid-template-columns: repeat(8, minmax(50px, 1fr));
   }
 `;
 
